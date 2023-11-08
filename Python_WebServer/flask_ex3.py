2cf718b62aea73653f58bb9ca20b8203a3e0bc95
leemyung <miung4130@naver.com>
2023-11-08 15:11:16 +0900

    flask

---
+++ Python_WebServer/flask_ex3.py
@@ -0,0 +1,25 @@
+import flask
+from value_object import Student
+
+app=flask.Flask(__name__)
+
+@app.route("/")
+def index():
+    return flask.render_template("lecture.html")
+
+@app.route("/sugang")
+def sugang():
+    return flask.render_template("sugang.html")
+
+@app.route("/student")
+def student():
+    students=[]
+    data={"students":students, "show_footer": True}
+    return flask.render_template("student_ex3.html", data=data)
+
+@app.route("/teacher")
+def teacher():
+    return flask.render_template("teacher.html")
+
+if __name__=="__main__":
+    app.run(host='0.0.0.0', port=8000, debug=True)
